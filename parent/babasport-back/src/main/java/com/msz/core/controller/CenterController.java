b3de63d09fcbe7b98061092cb304712f885df772
msz1990 <mashizhe1990@gmail.com>
2016-04-02 01:16:25 -0500

    first commit

---
+++ parent/babasport-back/src/main/java/com/msz/core/controller/CenterController.java
@@ -0,0 +1,14 @@
+package com.msz.core.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * backend center
+ * @author shizhema
+ *
+ */
+@Controller
+public class CenterController {
+	
+
+}
---
+++ parent/babasport-core/src/main/java/com/msz/core/web/Constants.java
@@ -0,0 +1,6 @@
+package com.msz.core.web;
+
+public class Constants {
+
+	public static final String USER_NAME="user_name";
+}
